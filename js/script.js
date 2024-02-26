0c8b6fe37214ecf974a3f9a6e2820f6d1448a962
Abhishek <138655413+Abhishek-kumar-sethi@users.noreply.github.com>
2024-02-26 20:05:52 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function work(){
+    var ul = document.querySelector("ul")
+    ul.classList.toggle("active")
+}
+function hello (){
+    var text = document.getElementById("text")
+console.log(text)
+}
+hello()
+// const textload = () =>{
+//     setTimeout(() =>{
+//         text.textContent = "Everthing";
+//     },0);
+//     setTimeout(() =>{
+//         text.textContent = "love";
+//     },4000);
+//     setTimeout(() =>{
+//         text.textContent = "addiction";
+//     },8000);
+//     setTimeout(() =>{
+//         text.textContent = "fun";
+//     },12000);
+// }
+// textload();
+// setInterval(textload,16000)
